045238017e00288b426e9497e7763d253f41fa12
byulrim <byulrim@gmail.com>
2023-04-06 01:30:39 +0900

    add command_count_by_id.py file

---
+++ command_count_by_id.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+df = pd.read_csv("data/command_data.csv")
+df_1 = df.groupby("id").count()
+df_1 = df_1.sort_values(by="command", ascending=False)
+
+df_1.rename(columns={"command": "command count"}, inplace=True)
+
+print(df_1.head(10))
+
+# print(df.groupby("id").count().sort_values(by="command", ascending=False).head(10))

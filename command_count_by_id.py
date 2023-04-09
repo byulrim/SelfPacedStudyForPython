7ff39c625cd25861283e64705d6bb4185118071e
byulrim <byulrim@gmail.com>
2023-04-09 11:02:26 +0900

    modify command_count_by_id.py - seventh commit.

--- command_count_by_id.py
+++ command_count_by_id.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 df = pd.read_csv("data/command_data.csv")
 df_1 = df.groupby("id").count()
-df_1 = df_1.sort_values(by="command", ascending=False)
+df_1 = df_1.sort_values("command", ascending=False)
 
 df_1.rename(columns={"command": "command count"}, inplace=True)
 
